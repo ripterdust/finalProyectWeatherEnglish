df592f4d5184a3baa6c105e2a94e9ed6a418def1
Bryan <bryantello2010@hotmail.com>
2021-10-09 15:14:38 -0600

    Agregada la barra de navegación

--- it3-app/src/App.js
+++ it3-app/src/App.js
@@ -1,9 +1,19 @@
-function App() {
-  return (
-    <div className="App">
-      Main page
-    </div>
-  );
+import { 
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import { MainPage } from './components/MainPage';
+import { Navbar } from './components/Navbar';
+
+
+export const App = () => {
+  return <Router>
+
+    <Navbar />
+    <Switch>
+      <Route exact path='/' component={ MainPage } />
+    </Switch>
+  </Router>
 }
 
-export default App;
