c82d86b72f02d7f5f59380260d6d1308714591cd
Bryan <bryantello2010@hotmail.com>
2021-10-09 16:15:13 -0600

    delete hook

--- it3-app/src/components/Solar.js
+++ it3-app/src/components/Solar.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import { useData } from '../hooks/useData'
+
 
 
 export const Solar = () => {
 
-    const data = useData('solar');
     return (
         <div>
             Solar
