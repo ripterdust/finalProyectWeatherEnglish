b78c40b52e2fdaf7418d9f2f6ee16c7f714b6b45
Bryan <bryantello2010@hotmail.com>
2021-10-09 15:19:48 -0600

    Agregado el enrutamiento y la barra de navegación

---
+++ it3-app/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+export const Navbar = () => {
+    return <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+            <Link className="navbar-brand" href="#">Navbar</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <NavLink activeClassName="active" className="nav-link" aria-current="page" exact to="/">Home</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink activeClassName="active" className="nav-link" to="/solar">Features</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink activeClassName="active" className="nav-link" to="/a">Pricing</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+}
