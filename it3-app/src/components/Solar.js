f329db48bae04cbbb642abc4c3fc7a55fc7a424f
Bryan <bryantello2010@hotmail.com>
2021-10-09 16:55:56 -0600

    Agregando la petición para mostrar todas las estaciones

--- it3-app/src/components/Solar.js
+++ it3-app/src/components/Solar.js
@@ -3,19 +3,25 @@ import { apiData } from '../apiData';
 
 export const Solar = () => {
 
-    const [data, setData] = useState();
+    const [data, setData] = useState({});
+
     const {key, baseUrl} = apiData;
 
-
     useEffect(() => {
-        fetch(`${baseUrl}pvwatts/v5.json?api_key=${key}`)
+
+        let request = `${baseUrl}solar/data_query/v1.json?api_key=${key}&lat=40&lon=-105&radius=50&all=1`;
+        console.log(request)
+        fetch(request)
             .then(res => res.json())
             .then(response => {
-                console.log(response)
+                let {outputs} = response;
+                let {all_stations} = outputs;
+                console.log(all_stations)
+                setData(all_stations);
             });
-    }, []);
+    }, [key, baseUrl]);
 
-
+    console.log(data)
     return (
         <div>
             Solar
