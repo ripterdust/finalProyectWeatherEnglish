8e09ffdea7e347d75dee72d0d80c6adebe9d86f4
Bryan <bryantello2010@hotmail.com>
2021-10-09 15:56:31 -0600

    agregado el apikey y el url del api

--- it3-app/src/components/Solar.js
+++ it3-app/src/components/Solar.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useData } from '../hooks/useData'
+
 
 export const Solar = () => {
+
+    const data = useData('solar');
     return (
         <div>
             Solar
