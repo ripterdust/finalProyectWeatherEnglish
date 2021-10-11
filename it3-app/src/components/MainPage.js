a275bc8956c4420d9afec92bbbbb3e10b750f848
Bryan <bryantello2010@hotmail.com>
2021-10-10 19:07:05 -0600

    Agregadas las tarjetas de la página del inicio

--- it3-app/src/components/MainPage.js
+++ it3-app/src/components/MainPage.js
@@ -1,9 +1,65 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export const MainPage = () => {
     return (
-        <div>
-            Main page
+        <div className="container animate__animated animate__fadeInUp">
+            <h6 className="text-center fs-1">A simple renewable energy tracker.</h6>
+            <div className="row container mt-4">
+
+                {/* Solar  */}
+                <div className="col-md-6">
+                    <div className="container mb-2">
+                        <div className="card container">
+                            <div className="card-body">
+                                <h5 className="card-title">Energy Type</h5>
+                                <p className="card-text">Track "energy type"</p>
+                                <Link className="btn btn-outline-primary" to="/solar">See more.</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Geo */}
+                <div className="col-md-6">
+                    <div className="container mb-2">
+                        <div className="card container">
+                            <div className="card-body">
+                                <h5 className="card-title">Energy Type</h5>
+                                <p className="card-text">Track "energy type"</p>
+                                <Link className="btn btn-outline-primary" to="/solar">See more.</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* wind */}
+                <div className="col-md-6">
+                    <div className="container mb-2">
+                        <div className="card container">
+                            <div className="card-body">
+                                <h5 className="card-title">Energy Type</h5>
+                                <p className="card-text">Track "energy type"</p>
+                                <Link className="btn btn-outline-primary" to="/solar">See more.</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                {/* hydro */}
+                <div className="col-md-6">
+                    <div className="container mb-2">
+                        <div className="card container">
+                            <div className="card-body">
+                                <h5 className="card-title">Energy Type</h5>
+                                <p className="card-text">Track "energy type"</p>
+                                <Link className="btn btn-outline-primary" to="/solar">See more.</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        
+                
+            </div>
         </div>
     )
 }
